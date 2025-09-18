3f89220ac2505c3bfba71d8a50f7acfb88fe1d9e
ManthanJain777 <Jainmanthan289@gmail.com>
2025-09-18 10:00:52 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App.tsx'
-import '../styles/globals.css'
+import App from './App.tsx' // Correct path now that it's in src
+import "../styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
